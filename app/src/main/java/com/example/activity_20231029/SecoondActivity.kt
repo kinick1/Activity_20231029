1e694be91aa2b3a87ada36a6a4a8129dde0490c2
kinick <kimjun9809@gmail.com>
2023-10-29 09:58:37 +0900

    SecondView finish로 화면을 끄고 이전의 화면을 다시 들어냄

--- app/src/main/java/com/example/activity_20231029/SecoondActivity.kt
+++ app/src/main/java/com/example/activity_20231029/SecoondActivity.kt
@@ -11,5 +11,15 @@ class SecoondActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding=DataBindingUtil.setContentView(this,R.layout.activity_secoond)
+        
+        
+        binding.btnFirstView.setOnClickListener { 
+            
+//            현재화면 종료: 밑에 깔린 이전 화면이 드러남
+            finish()
+//            Intent로 호출하면 안되는거냐? => Intent는 화면을 호출하는것 이므로 뒤로가기를 누르면 계속 추가된 화면이 나옴
+//            즉 화면이 계속 쌓이는것임
+            
+        }
     }
 }
