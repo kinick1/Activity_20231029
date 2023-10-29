5c3ab04c650f7e3de69a6bb403fd6deeb00e81f4
kinick <kimjun9809@gmail.com>
2023-10-29 09:55:32 +0900

    SecondView 별도로 바인딩

--- app/src/main/java/com/example/activity_20231029/SecoondActivity.kt
+++ app/src/main/java/com/example/activity_20231029/SecoondActivity.kt
@@ -2,10 +2,14 @@ package com.example.activity_20231029
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.example.activity_20231029.databinding.ActivitySecoondBinding
 
 class SecoondActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivitySecoondBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_secoond)
+        binding=DataBindingUtil.setContentView(this,R.layout.activity_secoond)
     }
 }
